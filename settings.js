621215ca1f0a22baeac12bc52a6f811bd99d09c2
Miftah0908 <99408827+miftah0908@users.noreply.github.com>
2023-04-19 14:59:54 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
-global.BOT_NAME = 'Hifumi OpenAI-BOT';
-global.BOT_TOKEN = 'ISI_TOKEN_BOT_DISINI';
+global.BOT_NAME = 'Zex Open AI';
+global.BOT_TOKEN = '6084977923:AAGR8dHBcopXwd6Jbq1ej_Ez87xJte2_IAI';
 global.MAX_TOKEN = 500;
-global.OWNER_NAME = 'Pixz4Youu';
-global.OWNER_USERNAME = 'Lvrtha';
-global.AI_TOKEN = 'ISI_APIKEY_OPENAI_DISINI'; //apikey disini: https://platform.openai.com/account/api-keys
+global.OWNER_NAME = 'Miftah GanzZ';
+global.OWNER_USERNAME = 'miftahganz';
+global.AI_TOKEN = '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'; //apikey disini: https://platform.openai.com/account/api-keys
 global.debug = true; //true or false
